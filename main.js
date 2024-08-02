c12cea28be0af30fb767667c4d696bc18c3361eb
Michael Yousef <michaelyousef@Michaels-MacBook-Air-4.local>
2024-08-02 14:57:15 -0500

    typewriter

---
+++ main.js
@@ -0,0 +1,36 @@
+let counter = 0;
+let content = [
+    'test1',
+    'test2',
+    'test3'
+]
+
+let jobTitle = document.querySelector('.typeWriter');
+
+const slide = () => {
+    if (counter >= content.length){
+        counter = 0;
+    }
+
+    jobTitle.innerHTML = '';
+    
+    jobTitle.classList.remove('holder-animation');
+    void jobTitle.offsetWidth;
+    jobTitle.classList.add('holder-animation');
+
+    const addLetterDiv = (letter, delay) => {
+        let letterDiv = document.createElement('div');
+        letterDiv.innerHTML = letter === " " ? "&nbsp" : letter;
+        letterDiv.classList.add('start', 'animation');
+        letterDiv.style.animationDelay = delay + 's';
+        jobTitle.appendChild(letterDiv);
+    };
+    
+    for(let i = 0; i < content[counter].length; i++){
+        addLetterDiv(content[counter][i], i / 10);
+    }
+
+    counter++;
+};
+
+slide();
